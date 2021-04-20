7d9472e73a51a20c6be61dfeed33730e929dbe93
Sergei Gaponik <s.gaponik@hedo-beauty.de>
2021-04-20 22:12:45 +0200

    1.3.2 updateOne

---
+++ src/core/signal.ts
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch'
+
+export async function sendSignal(collection: string, ids: string[]){
+
+  const url = `${process.env.SHOP_API_URL}/api`
+
+  const options = {
+    body: JSON.stringify({ collection, ids }),
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "POST"
+  }
+
+  const response = await fetch(url, options)
+
+  console.log(response)
+
+}
