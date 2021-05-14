f9742c3a0dc0891b931cd15581641a55a5ed3604
Sergei Gaponik <s.gaponik@hedo-beauty.de>
2021-05-14 13:11:15 +0200

    1.3.7 express apollo -> fastify mercurius

---
+++ src/core/initGraphQL.ts
@@ -0,0 +1,45 @@
+import { buildSchema } from 'type-graphql'
+import { ObjectId } from 'mongodb'
+import ObjectIdScalar from './ObjectIdScalar'
+import { InventoryResolver } from '../inventory/Inventory'
+import { StripeResolver } from '../payments/Stripe'
+import { CartResolver } from '../orders/Cart'
+import { AccessResolver } from '../access/Access'
+import * as path from 'path'
+import { PRODUCTION } from './const'
+import { AssetResolver, I18nResolver, InventoryItemResolver, LineItemResolver, OrderResolver, PaymentResolver, ProductResolver, SessionResolver, UserResolver, VariantResolver } from '@sergei-gaponik/hedo2.lib.models'
+import { FastifyInstance } from 'fastify'
+import mercurius from 'mercurius'
+
+export default async (app: FastifyInstance) => {
+
+  const schema = await buildSchema({ 
+    resolvers: [
+      InventoryResolver, 
+      InventoryItemResolver,
+      VariantResolver,
+      AssetResolver,
+      ProductResolver,
+      I18nResolver,
+      SessionResolver,
+      UserResolver,
+      OrderResolver,
+      PaymentResolver,
+      AccessResolver,
+      LineItemResolver,
+      CartResolver
+    ],
+    emitSchemaFile: path.resolve(__dirname, "../../schema.gql"),
+    scalarsMap: [
+      { 
+        type: ObjectId, 
+        scalar: ObjectIdScalar
+      }
+    ],
+  })
+
+  app.register(mercurius, {
+    schema,
+    graphiql: true
+  })
+}
